bbe4b0c32dea584a69e420ee63ed0fa0457f8611
camilolc <kmilo3025@hotmail.com>
2024-06-05 11:19:54 -0500

    firebase config implemented

---
+++ src/firebase/config.ts
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getAuth} from 'firebase/auth'
+
+import {getFirestore} from 'firebase/firestore'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBnprUk1szyp4rxB31HmgSBJE3-_5hZSv4",
+  authDomain: "play-green-c8721.firebaseapp.com",
+  projectId: "play-green-c8721",
+  storageBucket: "play-green-c8721.appspot.com",
+  messagingSenderId: "710934796646",
+  appId: "1:710934796646:web:b21418567b956e534b90a7"
+};
+
+// Initialize Firebase
+export const FirebaseApp = initializeApp(firebaseConfig);
+
+export const FirebaseAuth = getAuth(FirebaseApp);
+
+export const FirebaseDB = getFirestore(FirebaseApp);
---
+++ src/firebase/provider.ts
@@ -0,0 +1,27 @@
+import { signInWithEmailAndPassword} from 'firebase/auth';
+import { FirebaseAuth } from './config';
+
+interface Props{
+    email:string;
+    password:string
+}
+
+export const loginWithEmailPassword = async({ email, password }:Props) => {
+
+    try {
+        const resp = await signInWithEmailAndPassword( FirebaseAuth, email, password );
+        const { uid, photoURL, displayName } = resp.user;
+
+        return {
+            ok: true,
+            uid, photoURL, displayName
+        }
+
+    } catch (error:any) {
+        return { ok: false, errorMessage: error.message }
+    }
+}
+
+export const logoutFirebase = async() => {
+    return await FirebaseAuth.signOut();
+}
