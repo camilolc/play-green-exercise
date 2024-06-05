bf370e5088ffd2d6a3134405be77906e37a2f7d3
camilolc <kmilo3025@hotmail.com>
2024-06-05 11:24:37 -0500

    basic login form implemend, hooks for keeping user info auth

--- src/auth/layout/AuthLayout.tsx
+++ src/auth/layout/AuthLayout.tsx
@@ -1,3 +1,4 @@
+import { Grid, Typography } from "@mui/material"
 
 
 interface Props{
@@ -7,9 +8,33 @@ interface Props{
 
 export const AuthLayout = ({ children, title = '' }:Props) => {
   return (
-    <>
-        <h1>{title}</h1>
-        {children}
-    </>
+    <Grid
+    container
+    spacing={ 0 }
+    direction="column"
+    alignItems="center"
+    justifyContent="center"
+    sx={{ minHeight: '100vh', backgroundColor: 'primary.main', padding: 4 }}
+  >
+
+    <Grid item
+     className='box-shadow'
+     xs={ 3 }
+     sx={{ 
+          width: { sm: 450 },
+          backgroundColor: 'white', 
+          padding: 3, 
+          borderRadius: 2 
+      }}>
+        
+        <Typography variant='h5' sx={{ mb: 1 }}>{ title }</Typography>
+
+          
+          { children }
+          
+
+      </Grid>
+
+  </Grid>
   )
 }
